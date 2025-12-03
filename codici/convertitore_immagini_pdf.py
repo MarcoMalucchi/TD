bf42ff6f86508622c05f0d8ba30c99831f7d480d
Marco Malucchi <marco.malucchi.2003@gmail.com>
2025-12-03 16:06:46 +0100

    Task 4

--- codici/convertitore_immagini_pdf.py
+++ codici/convertitore_immagini_pdf.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-path = '/home/marco/Desktop/Uni_anno3/TD/Es_08/logbook/'
+path = '/home/marco/Desktop/Uni_anno3/TD/Es_09/logbook/'
 
-img = Image.open(path+"XOR_ingresso_uscita.png")
+img = Image.open(path+"fterzi_divider_task4_es09.png")
 img = img.convert("RGB")
-img.save(path+"XOR_ingresso_uscita.pdf")
+img.save(path+"Divisore_fterzi.pdf")
